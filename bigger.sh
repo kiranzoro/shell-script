08541956bd9bc93c1b4be7a7dc0122e1877b38c3
kiranzoro <kirankumar333444@gmail.com>
2024-04-20 23:25:25 +0530

    some-message

--- bigger.sh
+++ bigger.sh
@@ -2,5 +2,7 @@
 FIRST_NUMBER=$1
 SECOND_NUMBER=$2
 
-
-echo"$FIRST_NUMBER is Greater Than $SECOND_NUMBER"
+if [$FIRST_NUMBER -gt $SECOND_NUMBER]
+then
+    echo "$FIRST_NUMBER is Greater Than $SECOND_NUMBER"
+fi
