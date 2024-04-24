d01df4ba7011a378f0df68f6db8af267e1ad9466
kiranzoro <kirankumar333444@gmail.com>
2024-04-24 12:12:28 +0530

    Update

--- installation.sh
+++ installation.sh
@@ -4,10 +4,10 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPTNAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
-R="/e[31m"
-G="/e[32m"
-Y="/e[33m"
-N="/e[0m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 if [ $USERID != 0 ]
 then
