1e870d46ba97e6b6e34cbf76683915f99956b2ce
kiranzoro <kirankumar333444@gmail.com>
2024-04-26 09:48:05 +0530

    adding set command

--- delete-logs.sh
+++ delete-logs.sh
@@ -4,9 +4,10 @@ SOURCE_DIRECTORY=/tmp/app-logs
 
 if [ -d $SOURCE_DIRECTORY ]
 then
-    echo "echo Dir Exists"
+    echo "Directtory Exists"
 else
     echo "Please create Directory"
 fi
 
+cd /tmp/app-logs
 find . -name "*.log" +mtime +14
