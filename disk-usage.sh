86223669e94cf497ff7c697d555d698188422c1d
kiranzoro <kirankumar333444@gmail.com>
2024-04-27 12:03:31 +0530

    message

--- disk-usage.sh
+++ disk-usage.sh
@@ -3,7 +3,7 @@
 DISK_USAGE=$(df -hT)
 DISK_THRESHOLD=5
 
-while
+while IFS= read -r line
 do
     USAGE=$(df -hT | grep xfs | awk -F " " '{print $6F}'| cut -d "%" -f1)
     FOLDER=$(df -hT | grep xfs| awk -F " " '{print $NF}')
