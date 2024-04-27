13907bd3110d07568b596c1d1e4ed0b020ce6e77
kiranzoro <kirankumar333444@gmail.com>
2024-04-27 12:15:31 +0530

    message

--- disk-usage.sh
+++ disk-usage.sh
@@ -2,13 +2,12 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5
-echo "$DISK_USAGE"
 
 while IFS= read -r line
 do
     USAGE=$(echo $line |awk -F " " '{print $6F}'| cut -d "%" -f1)
-    FOLDER=$(echo $line| awk -F " " '{print $NF}')
-    if [ $USAGE >= $DISK_THRESHOLD ]
+    FOLDER=$(echo $line | awk -F " " '{print $NF}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
     then
         echo "memory of $FOLDER has been exceeded to $USAGE, Please clear the disk"
     fi
